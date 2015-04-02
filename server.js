6b6741d87ea78afac2f828698acc6781a2ed7101
Eddie Lin <elin@mozilla.com>
2015-04-02 20:25:49 +0800

    Allow cross domain access.

--- server.js
+++ server.js
@@ -5,6 +5,15 @@ var io = require('socket.io')(server);
 
 server.listen(2013);
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
 app.use(express.static('public'));
 app.post('/api/rub', function(req, res) {
   mdb(insertEvent, {'time': new Date(), 'action': 'rub'}, function(result) {
